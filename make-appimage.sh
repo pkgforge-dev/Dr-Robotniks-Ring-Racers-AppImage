533bfe23e6ed026695a9a02aab1052ab4557f92d
Link4Electronics <link4electronics@gmail.com>
2026-04-08 05:32:47 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,23 +3,19 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q ringracers | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/pixmaps/org.kartkrew.RingRacers.png
-export DESKTOP=/usr/share/applications/org.kartkrew.RingRacers.desktop
-export STARTUPWMCLASS=ringracers
+export ICON=https://raw.githubusercontent.com/KartKrewDev/RingRacers/refs/heads/master/srb2.png
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/ringracers
-
-# Additional changes can be done in between here
+quick-sharun ./AppDir/bin/ringracers
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
-# test the final app
-#quick-sharun --test ./dist/*.AppImage
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
