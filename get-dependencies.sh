0b40a2eac58046d6374e794401acaeeaf4691b9d
Link4Electronics <link4electronics@gmail.com>
2026-08-23 20:26:37 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,27 +6,35 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    cmake    \
-    libdecor \
-    libvpx   \
-    ninja    \
-    sdl2
+pacman -Syu --noconfirm cmake libvpx libyuv sdl2-compat
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-make-aur-package ringracers-data
-make-aur-package ringracers
+echo "Building stable version of Ring Racers..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/KartKrewDev/RingRacers"
+VERSION="$(curl -sL https://api.github.com/repos/KartKrewDev/RingRacers/releases/latest | grep '"tag_name"' | head -1 | cut -d '"' -f 4)"
+git clone --branch "$VERSION" --depth 1 "$REPO" ./RingRacers
+VERSION_NOV="${VERSION#v}"
+echo "$VERSION_NOV" > ~/version
 
-# If the application needs to be manually built that has to be done down here
+# Assets zip from same latest tag
+curl -L -o Dr.Robotnik.s-Ring-Racers-${VERSION}-Assets.zip \
+  "https://github.com/KartKrewDev/RingRacers/releases/download/$VERSION/Dr.Robotnik.s-Ring-Racers-${VERSION}-Assets.zip"
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+mkdir -p ./AppDir/share/games/RingRacers
+bsdtar -xvf Dr.Robotnik.s-Ring-Racers-${VERSION}-Assets.zip -C ./AppDir/share/games/RingRacers
+
+export CXXFLAGS="${CXXFLAGS:-} -Wp,-U_GLIBCXX_ASSERTIONS"
+cmake -S ./RingRacers -B build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_FLAGS="-g1 -O3" \
+    -DCMAKE_CXX_FLAGS=-"g1 -O3 -fpermissive" \
+    -DSRB2_CONFIG_DEV_BUILD=OFF \
+    -DSRB2_SDL2_EXE_NAME=ringracers \
+    -DACSVM_INSTALL_LIB=OFF
+cmake --build build -j$(nproc)
+mv -v build/bin/ringracers ./AppDir/bin
