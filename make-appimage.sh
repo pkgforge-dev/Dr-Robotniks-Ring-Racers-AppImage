6fd430be3db539cc70005d9cc7637085f02cd006
Link4Electronics <link4electronics@gmail.com>
2026-08-31 15:43:01 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,8 +8,6 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/KartKrewDev/RingRacers/refs/heads/master/srb2.png
-export DESKTOP=/usr/share/applications/org.kartkrew.RingRacers.desktop
-export STARTUPWMCLASS=ringracers
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
