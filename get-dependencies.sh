2c4c332cfe5764206723580827a20a2b2d5ee1ee
Link4Electronics <link4electronics@gmail.com>
2026-08-31 15:49:03 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -30,12 +30,12 @@ VERSION_NOV="${VERSION#v}"
 echo "$VERSION_NOV" > ~/version
 
 # AssetsLinuxOnly.zip from same latest tag
-curl -L -o Dr.Robotnik.s-Ring-Racers-$VERSION-Asset.zip \
-  "https://github.com/KartKrewDev/RingRacers/releases/download/$VERSION/Dr.Robotnik.s-Ring-Racers-$VERSION-Assets.zip"
+curl -L -o Dr.Robotnik.s-Ring-Racers-${VERSION}-Asset.zip \
+  "https://github.com/KartKrewDev/RingRacers/releases/download/$VERSION/Dr.Robotnik.s-Ring-Racers-${VERSION}-Assets.zip"
 
 mkdir -p ./AppDir/bin
 mkdir -p ./AppDir/share/games/RingRacers
-bsdtar -xvf Dr.Robotnik.s-Ring-Racers-$VERSION-Assets.zip -C ./AppDir/share/games/RingRacers
+bsdtar -xvf Dr.Robotnik.s-Ring-Racers-${VERSION}-Assets.zip -C ./AppDir/share/games/RingRacers
 
 cd ./RingRacers
 export CXXFLAGS="${CXXFLAGS:-} -Wp,-U_GLIBCXX_ASSERTIONS"
