70513c74beac5b0ff6dd740a89a4cbd2ca780663
Link4Electronics <link4electronics@gmail.com>
2026-08-31 15:29:11 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,18 +3,17 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q ringracers | awk '{print $2; exit}')
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/pixmaps/org.kartkrew.RingRacers.png
+export ICON=https://raw.githubusercontent.com/KartKrewDev/RingRacers/refs/heads/master/srb2.png
 export DESKTOP=/usr/share/applications/org.kartkrew.RingRacers.desktop
 export STARTUPWMCLASS=ringracers
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/ringracers
+quick-sharun ./AppDir/bin/ringracers
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
